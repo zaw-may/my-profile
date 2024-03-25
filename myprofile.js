b1b541f1b870e2b611b0c8763ab34b3a4777d69e
Zak <zawmay.data.analyst@gmail.com>
2024-03-25 22:18:52 +0700

    Profiling Project Section

--- myprofile.js
+++ myprofile.js
@@ -6,3 +6,12 @@ document.getElementById("requestCV").addEventListener("click", function () {
   
   window.location.href = email;
 });
+
+document.getElementById("linkToPBIX").addEventListener("click", function () {    
+  
+  var email = "mailto:zawmay.data.analyst@gmail.com" +
+  "?subject=Requesting%20Power%20BI%20Reports%20And%20Dashboards" +
+  "&body=" + encodeURIComponent("Hello Zaw, \n\nI am reaching out to express my interest in exploring your Power BI Web Services. Could you kindly provide details on the visualization capabilities available through your Power BI Web Services? I eagerly await your response.\n\nThank you!\n\nBest Regards,");
+  
+  window.location.href = email;
+});
