8314d73669f72e02f9040c159ef633aea63c15f6
Zak <zawmay.data.analyst@gmail.com>
2024-03-25 17:49:47 +0700

    Profiling Updated Link

--- myprofile.js
+++ myprofile.js
@@ -2,10 +2,7 @@ document.getElementById("requestCV").addEventListener("click", function () {
   
   var email = "mailto:zawmay.data.analyst@gmail.com" +
   "?subject=Requesting%20Updated%20CV" +
-  "&body=Hello Zaw, " +
-  "I'm interested in your skills and experiences. Please provide me your updated CV with more information. " +
-  "Thank you. " +
-  "Best Regards,";
+  "&body=" + encodeURIComponent("Hello Zaw,\n\n I'm interested in your skills and experiences. Please provide me your updated CV with more information.\n\nThank you!\n\nBest Regards,");
   
   window.location.href = email;
 });
